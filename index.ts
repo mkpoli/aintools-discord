66087839268f9909db75de8270d1f9d48f11e4fe
mkpoli <git@mkpo.li>
2024-11-01 01:02:47 +0900

    feat: add basic Discord bot

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits, Events } from 'discord.js';
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(TOKEN);
