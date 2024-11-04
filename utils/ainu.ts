6008d0e0766b321ca6479d424102a89552f6db38
mkpoli <git@mkpo.li>
2024-11-04 18:48:23 +0900

    feat: use auto segmentation provided by ainconv@0.4.0

--- utils/ainu.ts
+++ utils/ainu.ts
@@ -4,23 +4,13 @@ export function convertSentence(
 	text: string,
 	converter: (word: string) => string,
 ) {
-	const words = text
-		.toLowerCase()
-		.split(/(\p{L}+)/u)
-		.filter(Boolean);
-	const convertedWords = words.map((word) => {
-		if (word.match(/\s+/)) {
-			return word;
-		}
-		try {
-			return converter(word);
-		} catch (e) {
-			logger.error("[utils/ainu] {word} -> {error}", {
-				word,
-				error: e.message,
-			});
-			return word;
-		}
-	});
-	return convertedWords.join("");
+	try {
+		return converter(text);
+	} catch (e) {
+		logger.error("[utils/ainu] {text} -> {error}", {
+			text,
+			error: e instanceof Error ? e.message : String(e),
+		});
+		return text;
+	}
 }
